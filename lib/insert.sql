cc449688f28dcc8bb33fd6d1e13455449f932971
cindych419@gmail.com <cindych419@gmail.com>
2018-05-29 23:26:56 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears('Mr.Chocolate', 4, 'F', 'BLUE', 'GOOD', 0)
+INSERT INTO bears('Rowdy', 4, 'F', 'BLUE', 'GOOD', 0)
+INSERT INTO bears('Tabitha', 4, 'F', 'BLUE', 'GOOD', 0)
+INSERT INTO bears('Sergeant Brown', 4, 'F', 'BLUE', 'GOOD', 0)
+INSERT INTO bears('Melissa', 4, 'F', 'BLUE', 'GOOD', 0)
+INSERT INTO bears('Grinch', 4, 'F', 'BLUE', 'GOOD', 0)
+INSERT INTO bears('Wendy', 4, 'F', 'BLUE', 'GOOD', 0)
+INSERT INTO bears( NULL, 4, 'F', 'BLUE', 'GOOD', 0)
