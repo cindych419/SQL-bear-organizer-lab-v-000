61adca24147310f6481b9f4c4a21384dffc2fda9
cindych419@gmail.com <cindych419@gmail.com>
2018-05-29 23:30:42 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears('Mr.Chocolate', 4, 'F', 'BLUE', 'GOOD', 0)
-INSERT INTO bears('Rowdy', 4, 'F', 'BLUE', 'GOOD', 0)
-INSERT INTO bears('Tabitha', 4, 'F', 'BLUE', 'GOOD', 0)
-INSERT INTO bears('Sergeant Brown', 4, 'F', 'BLUE', 'GOOD', 0)
-INSERT INTO bears('Melissa', 4, 'F', 'BLUE', 'GOOD', 0)
-INSERT INTO bears('Grinch', 4, 'F', 'BLUE', 'GOOD', 0)
-INSERT INTO bears('Wendy', 4, 'F', 'BLUE', 'GOOD', 0)
-INSERT INTO bears( NULL, 4, 'F', 'BLUE', 'GOOD', 0)
+INSERT INTO bears (name, age, gender,color, temperament, alive ) VALUES ('Mr.Chocolate', 4, 'F', 'BLUE', 'GOOD', 0);
+INSERT INTO bears (name, age, gender,color, temperament, alive ) VALUES ('Rowdy', 4, 'F', 'BLUE', 'GOOD', 0);
+INSERT INTO bears (name, age, gender,color, temperament, alive ) VALUES ('Tabitha', 4, 'F', 'BLUE', 'GOOD', 0);
+INSERT INTO bears (name, age, gender,color, temperament, alive ) VALUES ('Sergeant Brown', 4, 'F', 'BLUE', 'GOOD', 0);
+INSERT INTO bears (name, age, gender,color, temperament, alive ) VALUES ('Melissa', 4, 'F', 'BLUE', 'GOOD', 0);
+INSERT INTO bears (name, age, gender,color, temperament, alive ) VALUES ('Grinch', 4, 'F', 'BLUE', 'GOOD', 0);
+INSERT INTO bears (name, age, gender,color, temperament, alive ) VALUES ('Wendy', 4, 'F', 'BLUE', 'GOOD', 0);
+INSERT INTO bears (name, age, gender,color, temperament, alive ) VALUES ( NULL, 4, 'F', 'BLUE', 'GOOD', 0);
